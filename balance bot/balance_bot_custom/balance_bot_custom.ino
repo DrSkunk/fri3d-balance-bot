23cca38b74f03193754a3bf3d8447ef1dea6b532
Sebastiaan Jansen <whitebird@live.Be>
2018-08-16 16:51:29 +0200

    kalman filter

--- balance bot/balance_bot_custom/balance_bot_custom.ino
+++ balance bot/balance_bot_custom/balance_bot_custom.ino
@@ -1,7 +1,6 @@
 #include "PID_v1.h"
 #include "LMotorController.h"
 #include "I2Cdev.h"
-//#include "MPU6050_6Axis_MotionApps20.h"
 #include "MPU6050.h"
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
     #include "Wire.h"
@@ -14,8 +13,14 @@ MPU6050 mpu;
 // MPU control/status vars
 uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
 // orientation/motion vars
-
 float pitch = 0;
+
+// Smoothing
+const int numReadings = 10;
+float readings[numReadings];
+int readIndex = 0;
+float total = 0;
+float average = 0;
 
 //PID
 double originalSetpoint = 0.0;
@@ -43,10 +48,6 @@ LMotorController motorController(ENACHANNEL, IN1, ENBCHANNEL, IN2, motorSpeedFac
 //timers
 long time1Hz = 0;
 long time5Hz = 0;
-
-
-
-
 
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
@@ -116,17 +117,39 @@ void setup()
 
 
 void loop()
-{
-      // pitch berekenen
+{  
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  //input = -(atan2(ax, sqrt(ay*ay + az*az))*180.0)/M_PI;
 
-        mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-        input = -(atan2(ax, sqrt(ay*ay + az*az))*180.0)/M_PI;
-        //Serial.print("Input ");
-        //Serial.println(input);
-        pid.Compute();
-        //Serial.print("Output ");
-        //Serial.println(output);
-        motorController.move(output, MIN_ABS_SPEED);
+  // subtract the last reading:
+  total = total - readings[readIndex];
+  // read from the sensor:
+  readings[readIndex] = -(atan2(ax, sqrt(ay*ay + az*az))*180.0)/M_PI;
+  // add the reading to the total:
+  total = total + readings[readIndex];
+  // advance to the next position in the array:
+  readIndex = readIndex + 1;
+
+  // if we're at the end of the array...
+  if (readIndex >= numReadings) {
+    // ...wrap around to the beginning:
+    readIndex = 0;
+  }
+
+  // calculate the average:
+  // average = total / numReadings;
+  input = total / numReadings;
+  // send it to the computer as ASCII digits
+  Serial.println(average);
+  delay(1);        // delay in between reads for stability
+
+  
+  //Serial.print("Input ");
+  //Serial.println(input);
+  pid.Compute();
+  //Serial.print("Output ");
+  //Serial.println(output);
+  motorController.move(output, MIN_ABS_SPEED);
 }
 
 
